80d0177108e27b9a0195347de186f02bdd4299de
pacafuentes <fuentespaca@gmail.com>
2016-07-19 15:28:09 -0300

    show opponent on game

--- app.js
+++ app.js
@@ -32,6 +32,7 @@ app.use('/', routes);
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
+  // res.set('Location', '/home').status(307).send();
   next(err);
 });
 
--- controllers/game.js
+++ controllers/game.js
@@ -1,4 +1,5 @@
 var Game = require('../models/Game');
+var User = require('../models/User');
 
 module.exports = {
   create: function (req, res) {
@@ -6,8 +7,10 @@ module.exports = {
     Game.findOne({'id': gameId, 'finished': false, $or : [{player1Id: req.session.user.id}, {player2Id: req.session.user.id}]}, function (err, game) {
       console.log(game);
       if (game) {
-        res.cookie('gameId', gameId);
-        res.render('game', {user: req.session.user});
+        User.findOne({'id': game.getOpponentId(req.session.user.id)}, function (err, user) {
+          res.cookie('gameId', gameId);
+          res.render('game', {user: req.session.user, opponent: user});
+        })
       }
       else res.set('Location', '/home').status(307).send();
     });
